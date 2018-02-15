13db01d703f779fb07bb21aede86cde14483c96d
Sripaad srinivasan <sripaad751@gmail.com>
2018-02-15 14:55:22 +0530

    create whois.py

---
+++ whois.py
@@ -0,0 +1,16 @@
+import os
+
+
+def get_whois(url):
+    '''
+    Function to get whois of the url
+    :param url: url 
+    :return: whois result 
+    '''
+    command = 'whois ' + url
+
+    process = os.popen(command)
+
+    result = str(process.read())
+
+return result
